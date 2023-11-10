bf84775060e0017b6fc56429f00793c02ab68478
muhammadaafiff <muhammad.afif250302@gmail.com>
2023-11-11 02:53:32 +0700

    mengisi data tabel pelanggan

--- membuat_pelanggan.sql
+++ membuat_pelanggan.sql
@@ -2,7 +2,7 @@ CREATE TABLE pelanggan (
 id VARCHAR(5) PRIMARY KEY,
 nama VARCHAR(100) NOT NULL,
 email VARCHAR(100) NOT NULL UNIQUE,
-no_hp INT(13) NOT NULL,
+no_hp VARCHAR(100) NOT NULL,
 jenis_kelamin ENUM('laki-laki', 'perempuan'),
 alamat VARCHAR(100)
 );
---
+++ mengisi_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO pelanggan (id, nama, email, no_hp, jenis_kelamin, alamat) VALUES
+('C1', 'Andri', 'andri@gmail.com', '081156789021', 'laki-laki', 'Bandung'),
+('C2', 'Budi', 'budi@gmail.com', '081712345634', 'laki-laki', 'Semarang'),
+('C3', 'Tono', 'tono@gmail.com', '081665432169', 'laki-laki', 'Madura'),
+('C4', 'Cahya', 'cahya@gmail.com', '081234567898', 'perempuan', 'Surabaya'),
+('C5', 'Dodo', 'dodo@gmail.com', '085212345602', 'laki-laki', 'Padang'),
+('C6', 'Frank', 'frank@gmail.com', '085365432111', 'laki-laki', 'Solo'),
+('C7', 'Kim', 'kim@gmail.com', '085123456743', 'perempuan', 'Jakarta'),
+('C8', 'Lubis', 'lubis@gmail.com', '085543210900', 'laki-laki', 'Aceh'),
+('C9', 'Rosa', 'rosa@gmail.com', '081345678911', 'perempuan', 'Bali'),
+('C10', 'Viona', 'viona@gmail.com', '081234567832', 'perempuan', 'Malang')
+
