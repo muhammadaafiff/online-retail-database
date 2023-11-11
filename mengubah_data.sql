aa01e453e2dd4247619a0e25c02ba16d1e730995
muhammadaafiff <muhammad.afif250302@gmail.com>
2023-11-11 23:37:43 +0700

    mengubah data pada tabel

--- mengubah_data.sql
+++ mengubah_data.sql
@@ -2,4 +2,8 @@ UPDATE produk
 SET harga=130000, stok=160 
 WHERE id='P002';
 
+UPDATE kategori_produk
+SET deskripsi="untuk produk-produk yang memerlukan listrik"
+WHERE id='K3';
+
 SELECT * FROM produk; --untuk melihat perubahan data pada tabel
